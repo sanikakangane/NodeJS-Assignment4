0d54ead00756d87a1c46b6a48ef3ce2077d42f8c
Sanika Kangane <sanikakangane166@gmail.com>
2026-08-25 15:00:14 +0530

    Refactor file operations for better readability

--- Assignment4/index.js
+++ Assignment4/index.js
@@ -1,54 +1,54 @@
 const fs = require("fs");
 
 fs.writeFile(
-  "student.txt",
-  "Name: Sanika Kangane\nCourse: Full Stack Development\nTechnology: Node.js",
-  (err) => {
+    "student.txt",
+    "Name: Sanika Kangane\nCourse: Full Stack Development\nTechnology: Node.js",
+    (err) => {
     if (err) {
-      console.log("Error while creating file");
-      return;
+    console.log("Error while creating file");
+    return;
     }
 
     console.log("File created successfully");
 
     fs.readFile("student.txt", "utf8", (err, data) => {
-      if (err) {
+    if (err) {
         console.log("Error while reading file");
         return;
-      }
+    }
 
-      console.log(data);
+    console.log(data);
 
-      fs.appendFile(
+    fs.appendFile(
         "student.txt",
         "\nExperience: 1 Year\nCity: Kolkata",
         (err) => {
-          if (err) {
+        if (err) {
             console.log("Error while updating file");
             return;
-          }
+        }
 
-          console.log("Data updated successfully");
+        console.log("Data updated successfully");
 
-          fs.rename("student.txt", "studentDetails.txt", (err) => {
+        fs.rename("student.txt", "studentDetails.txt", (err) => {
             if (err) {
-              console.log("Error while renaming file");
-              return;
+            console.log("Error while renaming file");
+            return;
             }
 
             console.log("File renamed successfully");
 
             fs.unlink("studentDetails.txt", (err) => {
-              if (err) {
+            if (err) {
                 console.log("Error while deleting file");
                 return;
-              }
+            }
 
-              console.log("File deleted successfully");
+            console.log("File deleted successfully");
             });
-          });
+        });
         }
-      );
+    );
     });
-  }
+}
 );
