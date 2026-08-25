d004aa6fdbb468d6254c5ccda0928bae7ffdf5d5
Sanika Kangane <sanikakangane166@gmail.com>
2026-08-25 14:57:54 +0530

    Update index.js

--- Assignment4/index.js
+++ Assignment4/index.js
@@ -1,9 +1,54 @@
 const fs = require("fs");
 
-fs.unlink("studentDetails.txt", (err) => {
+fs.writeFile(
+  "student.txt",
+  "Name: Sanika Kangane\nCourse: Full Stack Development\nTechnology: Node.js",
+  (err) => {
     if (err) {
-    console.log("Error while deleting file");
-    return;
-}
-console.log("File deleted successfully");
-});
+      console.log("Error while creating file");
+      return;
+    }
+
+    console.log("File created successfully");
+
+    fs.readFile("student.txt", "utf8", (err, data) => {
+      if (err) {
+        console.log("Error while reading file");
+        return;
+      }
+
+      console.log(data);
+
+      fs.appendFile(
+        "student.txt",
+        "\nExperience: 1 Year\nCity: Kolkata",
+        (err) => {
+          if (err) {
+            console.log("Error while updating file");
+            return;
+          }
+
+          console.log("Data updated successfully");
+
+          fs.rename("student.txt", "studentDetails.txt", (err) => {
+            if (err) {
+              console.log("Error while renaming file");
+              return;
+            }
+
+            console.log("File renamed successfully");
+
+            fs.unlink("studentDetails.txt", (err) => {
+              if (err) {
+                console.log("Error while deleting file");
+                return;
+              }
+
+              console.log("File deleted successfully");
+            });
+          });
+        }
+      );
+    });
+  }
+);
